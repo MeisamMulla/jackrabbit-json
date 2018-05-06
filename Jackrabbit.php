a8c15471c5cec571b2c2caea2ef436d9ae80a009
Meisam Mulla <mmeisam@gmail.com>
2018-05-06 01:10:02 -0700

    Initial Commit

---
+++ Jackrabbit.php
@@ -0,0 +1,135 @@
+<?php
+/** 
+ *   Copyright (c) 2018 Meisam Mulla <http://meisam.mulla.ca>
+ *   
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *   
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ **/
+namespace MeisamMulla;
+
+class Jackrabbit
+{
+    private $orgID;
+    private $endpoint = 'https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJSON?';
+    private $cacheTime = '-5 minutes';
+
+    public function __construct(string $orgID)
+    {
+        $this->orgID = $orgID;
+    }
+
+    public function query(array $parameters) : array 
+    {
+        $url = $this->generateUrl($parameters);
+
+        return $this->getJSON($url);
+    }
+
+    public function generateUrl(array $parameters) : string
+    {
+        $parameters['OrgID'] = $this->orgID;
+
+        return $this->endpoint . http_build_query($parameters);
+    }
+
+    private function getJSON(string $url) : array
+    {
+        $filename = dirname(__FILE__) . '/dance-cache/' . md5($url) . '.txt';
+
+        if (!file_exists($filename) || filemtime($filename) < strtotime($this->cacheTime)) {
+            return $this->storeJSON($url);
+        }
+
+        return json_decode(file_get_contents($filename));
+    }
+
+    private function storeJSON($url)
+    {
+        $classes = json_decode(file_get_contents($url));
+        $newArray = [];
+
+        if (!$classes->success) {
+            throw new Exception('Invalid record set');
+        }
+
+        foreach ($classes->rows as $class) {
+            $newArray[] = [
+                'name' => $class->name,
+                'description' => $class->description,
+                'location' => ucfirst(strtolower($class->location)),
+                'ages' => $this->getAge($class->min_age, $class->max_age),
+                'day' => $this->getDays($class->meeting_days),
+                'link' => $class->online_reg_link,
+                'openings' => $class->openings->calculated_openings,
+                'times' => $this->convertTime($class->start_time) . '-' . $this->convertTime($class->end_time),
+                'dates' => $this->convertDate($class->start_date) . '-' . $this->convertDate($class->end_date),
+            ];
+        }
+
+        $fp = fopen(dirname(__FILE__) . '/dance-cache/' . md5($url) . '.txt', 'w+');
+        fwrite($fp, json_encode($newArray));
+        fclose($fp);
+
+        return json_decode(json_encode($newArray));
+    }
+
+
+    private function getAge(string $min, string $max) : string
+    {
+        return $this->parseAge($min) . ' - ' . $this->parseAge($max);
+    }
+
+    private function parseAge(string $string) : string
+    {
+        $date = explode('Y', $string);
+
+        if (!isset($date['1'])) {
+            return '';
+        }
+
+        $years = ltrim(preg_replace('/[^0-9]/', '', $date['0']), '0');
+        $month = ltrim(preg_replace('/[^0-9]/', '', $date['1']), '0');
+
+        if ($month >= 6) {
+            $years += 1;
+        }
+
+        return $years;
+    }
+
+    private function getDays(stdClass $days) : string
+    {
+        foreach ($days as $key => $value) {
+            if ($value) {
+                return date('l', strtotime($key));
+            }
+        }
+
+        return false;
+    }
+
+    private function convertTime(string $time) : string
+    {
+        return date('g:ia', strtotime($time));
+    }
+
+    private function convertDate(string $date) : string
+    {
+        return date('M jS', strtotime($date));
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# JackRabbit JSON Class Listings
+This class retrieves Jackrabbit class listings and returns them in an easier to handle format for displaying the listings on a website.
+
+## Usage
+Initialize the class by passing the Organization ID as a parameter in the constructor:
+```php
+$jr = new MeisamMulla\Jackrabbit('123456');
+```
+
+Then use the `query()` method to get the resuts of your requests. The method accepts an array of parameters and those parameters can be found in the [JackRabbit documentation](http://jackrabbitcarehelp.com/guide2/default.aspx?pageid=add-columns).
+
+As an example:
+```php
+$jr = new MeisamMulla\Jackrabbit('123456');
+
+$json = $jr->query([
+    'Session' => '2018/19',
+    'loc' => 'LOC1',
+    'cat1' => 'Ballet',
+]);
+```
+
+Will return an output similar to this one:
+
+```json
+[
+    {  
+        "name":"Duo Ballet",
+        "description":"All Ballet Duo Students",
+        "location":"Studio Location",
+        "ages":"5 - 16",
+        "day":"Friday",
+        /* The link will be empty if Allow Registration is set to No inside JackRabbit portal */
+        "link":"https:\/\/app.jackrabbitclass.com\/reg.asp?id=123456&amp;hc=&amp;initEmpty=&amp;hdrColor=&amp;WL=0&amp;preLoadClassID=000000&amp;loc=",
+        "openings":0,
+        "times":"9:00am-9:45am",
+        "dates":"Jan 1st-Jun 1st"
+   }
+]
+```
+
+### Caching
+The results will be cached in the `dance-cache` directory for 5 minutes by default. You can modify this behaviour by changing the `$cacheTime` property in the class. This value can be written in human readable english (ex. `-30 minutes` for a cache time of 30 minutes).
