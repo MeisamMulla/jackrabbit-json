66f180ea7e058108fc77b21df874fbde98bc5b6e
Meisam Mulla <mmeisam@gmail.com>
2018-05-06 01:45:12 -0700

    Added Example

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+require 'Jackrabbit.php';
+
+$jr = new MeisamMulla\Jackrabbit('000000');
+
+try {
+    $rows = $jr->query([
+        'Session' => '2018/19',
+        'loc' => 'RIDGE',
+        'cat1' => 'Ballet',
+    ]);
+
+    foreach ($rows as $row) {
+        echo $row->name . "\n";
+    }
+
+} catch(Exception $e) {
+    echo $e;
+}
